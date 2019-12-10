47bd9f6e3d8f349211f5b3c3363ea5430b78dc38
BenoitRanque <ranque.benoit@gmail.com>
2019-12-10 00:19:48 -0400

    added project template

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-btn @click="print">Print</q-btn>
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  methods: {
+    print () {
+      // see here!
+      // https://stackoverflow.com/questions/37627064/how-to-print-a-div-in-electronjs
+    }
+  }
+}
+</script>
